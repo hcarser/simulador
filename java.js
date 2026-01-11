86ad933c25cc76ecb4a27760ff4f3b03c0b36018
Humberto Cárdenas <72759373+hcarser@users.noreply.github.com>
2026-01-11 13:18:11 -0600

    ACTUALZAR AA

--- java.js
+++ java.js
@@ -241,7 +241,7 @@ const TRANSLATIONS = {
         let currentCart = {};
         
         async function callGemini(prompt, schema = null) {
-            const apiKey = "AIzaSyDUft77jEIeTBaVkU97ME04PeXFysJQVEc";
+            const apiKey = "  ";
             const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=${apiKey}`;
             const payload = { contents: [{ role: "user", parts: [{ text: prompt }] }] };
             if (schema) {
@@ -634,4 +634,5 @@ El mensaje del usuario es: "${question}"`;
             }
         });
         
+
         animate();
